4d0adbb9455f7b872dd0aaa7f3cf5c53d07eab87
Clauderic Demers <me@ced.io>
2016-06-05 14:47:06 -0400

    Tweak padding for year selection list

--- src/Years/index.js
+++ src/Years/index.js
@@ -18,6 +18,12 @@ export default class Years extends Component {
         theme: PropTypes.object,
         years: PropTypes.array
     };
+    componentDidMount() {
+        let vs = this.refs.VirtualScroll;
+		let grid = vs && vs.refs.Grid;
+
+		this.scrollEl = grid && grid.refs.scrollingContainer;
+    }
     handleClick(year) {
         let {hideYearsOnSelect, maxDate, minDate, onDaySelect, scrollToDate, selectedDate, setDisplay} = this.props;
         let date = selectedDate || moment();
@@ -40,12 +46,16 @@ export default class Years extends Component {
         });
     }
     render() {
-        let {height, selectedDate, theme, years, width} = this.props;
+        let {height, selectedDate, theme, width} = this.props;
         const currentYear = moment().year();
+        let years = this.props.years.slice(0, this.props.years.length);
+        // Add spacer rows at the top and bottom
+        years.unshift(null);
+        years.push(null);
+
         let selectedYearIndex = years.indexOf((selectedDate) ? selectedDate.year() : currentYear);
         const rowHeight = 50;
         const containerHeight = (years.length * rowHeight < height + 50) ? years.length * rowHeight : height + 50;
-
 
         return (
             <div
@@ -53,29 +63,36 @@ export default class Years extends Component {
                 style={{color: theme.selectionColor, height: height + 50}}
             >
                 <VirtualScroll
+                    ref="VirtualScroll"
                     className={style.list}
                     width={width}
                     height={containerHeight}
                     rowCount={years.length}
                     rowHeight={rowHeight}
-                    scrollTop={selectedYearIndex * rowHeight - height/2}
+                    scrollToIndex={selectedYearIndex + 1}
+                    scrollToAlignment={'center'}
                     rowRenderer={({index}) => {
                         let year = years[index];
-                        let isActive = (index == selectedYearIndex);
 
-                        return (
-                            <div
-                                className={classNames(style.year, {[style.active]: isActive, [style.currentYear]: (year == currentYear)})}
-                                onClick={() => this.handleClick(year)}
-                                title={`Set year to ${year}`}
-                                data-year={year}
-                                style={{color: (typeof theme.selectionColor == 'function') ? theme.selectionColor(selectedDate.clone().year(year)) : theme.selectionColor}}
-                            >
-                                <span style={(year == currentYear) ? {borderColor: theme.todayColor} : null}>
-                                    {year}
-                                </span>
-                            </div>
-                        );
+                        if (year !== null) {
+                            let isActive = (index == selectedYearIndex);
+
+                            return (
+                                <div
+                                    className={classNames(style.year, {[style.active]: isActive, [style.currentYear]: (year == currentYear)})}
+                                    onClick={() => this.handleClick(year)}
+                                    title={`Set year to ${year}`}
+                                    data-year={year}
+                                    style={{color: (typeof theme.selectionColor == 'function') ? theme.selectionColor(selectedDate.clone().year(year)) : theme.selectionColor}}
+                                >
+                                    <span style={(year == currentYear) ? {borderColor: theme.todayColor} : null}>
+                                        {year}
+                                    </span>
+                                </div>
+                            );
+                        } else {
+                            return <div className={style.spacer}></div>;
+                        }
                     }}
                 />
             </div>
