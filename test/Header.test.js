2a01765996da849ea4311b0b0c181d65bff4f719
Clauderic Demers <me@ced.io>
2016-06-04 22:55:36 -0400

    Removed tests that were no longer relevant

--- test/Header.test.js
+++ test/Header.test.js
@@ -24,20 +24,6 @@ describe("<Header/>", () => {
         expect(wrapper.find(`.${style.root}`).hasClass(style.blank)).to.equal(true);
         expect(wrapper.text()).to.equal(defaultLocale.blank);
 	})
-    it('fires a callback when clicked on', () => {
-		const onClick = sinon.spy();
-        const wrapper = shallow(<Header selectedDate={moment()} onClick={onClick} locale={defaultLocale}/>);
-
-        wrapper.simulate('click');
-        expect(onClick.calledOnce).to.equal(true);
-	})
-    it('does not fire a callback when clicked on if there is no selectedDate', () => {
-		const onClick = sinon.spy();
-        const wrapper = shallow(<Header onClick={onClick} locale={defaultLocale}/>);
-
-        wrapper.simulate('click');
-        expect(onClick.called).to.equal(false);
-	})
     it('honors the current theme', () => {
         const wrapper = mount(<Header theme={{headerColor: 'red', textColor: {active: 'blue'}}} locale={defaultLocale}/>);
         const rootEl = wrapper.find(`.${style.root}`);
