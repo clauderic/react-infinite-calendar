0040ab2c1d1196fc10521e2a8e1f23c4b3470233
clauderic <clauderic.demers@shopify.com>
2017-02-24 19:43:03 -0500

    Import styles in demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import InfiniteCalendar from '../../src';
+import '../../styles.css';
 import './demo.css';
 
 render(<InfiniteCalendar />, document.querySelector('#demo'));
