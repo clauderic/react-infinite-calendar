4abfff9e1f970e14f9ac4a4aec3b738d32714bc0
Ethan <mailethanshao@gmail.com>
2018-04-03 11:43:54 +0800

    Add range style and funciton

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,7 @@ import isBefore from 'date-fns/is_before';
 import isSameDay from 'date-fns/is_same_day';
 import endOfDay from 'date-fns/end_of_day';
 import startOfDay from 'date-fns/start_of_day';
+import parse from 'date-fns/parse';
 import {withPropsOnChange} from 'recompose';
 
 export const keyCodes = {
@@ -185,5 +186,19 @@ export function range(start, stop, step = 1) {
 
   return range;
 };
+
+export function isRange(date) {
+    if (!date) {
+        return false;
+    }
+    const {start, end} = date;
+    return start !== undefined && end !== undefined;
+}
+
+export function getSortedDate(start, end) {
+  return isBefore(start, end)
+    ? {start, end}
+    : {start: end, end: start};
+}
 
 export {default as animate} from './animate';
